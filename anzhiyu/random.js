c12361142b9db0c4fccdf17b830bbe93b5d3dabb
Jonny0511 <guanyiqiao@qq.com>
2024-11-02 10:31:44 +0800

    Site updated: 2024-11-02 10:31:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/01/猜数字小游戏/","2024/11/01/毒液迎来最后一舞/","2024/11/01/算法数据结构-关于单调栈/"];function toRandomPost(){
+var posts=["2024/11/01/毒液迎来最后一舞/","2024/11/01/算法数据结构-关于单调栈/","2024/11/01/猜数字小游戏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
